ef697eb9ed30ea7a3590072cf8237ab4cb90d3d7
mgrinzPlayer <mgrinzPlayer@users.noreply.github.com>
2016-02-18 13:28:21 +0100

    out-of-order modification
    out-of-order modification

--- Cheat Engine/memscan.pas
+++ Cheat Engine/memscan.pas
@@ -1169,6 +1169,7 @@ function TGroupData.compareblock_outoforder(newvalue,oldvalue: pointer): boolean
 var i,j: integer;
   currentoffset: integer;
   isin: boolean;
+  currentoffsetEqualToZeroExist: boolean;
 
 function isinlist: boolean; //check if currently in the list of offsets, if so, return true and adjust the current offset so the scan starts from next offset
 var c: integer;
@@ -1188,6 +1189,7 @@ end;
 
 begin
   result:=true;
+  currentoffsetEqualToZeroExist:=false;
   for i:=0 to groupdatalength-1 do
   begin
     if result=false then exit;
@@ -1306,7 +1308,11 @@ begin
 
     end;
     groupdata[i].offset:=currentoffset-1;
+    if groupdata[i].offset=0 then currentoffsetEqualToZeroExist:=true;
   end;
+
+  if result and outoforder then // at least one current offset must be zero
+    result:=currentoffsetEqualToZeroExist;
 end;
 
 
@@ -4468,8 +4474,6 @@ begin
       FoundBufferSize:=0;
 
       variablesize:=groupdata.blocksize;   //this is why there is no nextscan data to compare against (varsize of 4096)
-      if groupdata.outoforder and groupdata.outoforder_aligned then
-        fastscanalignsize:=variablesize; //else use the given alignment
 
       if scannernr=0 then //write the header for groupdata (after the normal header comes the number of offsets)
         Addressfile.WriteBuffer(groupdata.groupdatalength, sizeof(groupdata.groupdatalength));
